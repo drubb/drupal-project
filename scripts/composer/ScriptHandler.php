252e678e76de4bebfef1c83af48b60a1c9eec508
Boris Böhne <drubb@users.noreply.github.com>
2017-03-12 14:45:48 +0100

    Changed a folder name

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -34,7 +34,7 @@ class ScriptHandler
             "$web_root/modules/custom",
             "$web_root/profiles/contrib",
             "$web_root/profiles/custom",
-            "$web_root/themes/custom",
+            "$web_root/themes/contrib",
             "$web_root/themes/custom",
             "$project_root/drush/contrib",
             "$project_root/drush/custom",
